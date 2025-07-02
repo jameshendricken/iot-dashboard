e769ecbfdc3775d86a3bdf1d5d182f6273d0a1d9
jameshendricken <jameshendricken@gmail.com>
2025-07-02 01:45:51 +0100

    removrd localhost from DeviceData

--- frontend/src/DeviceData.js
+++ frontend/src/DeviceData.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 
-const API_URL = process.env.REACT_APP_API_BASE || "http://localhost:8000";
+const API_URL = process.env.REACT_APP_API_BASE;
 
 export default function DeviceData() {
   const [deviceIds, setDeviceIds] = useState([]);
