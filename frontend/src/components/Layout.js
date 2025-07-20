dbab373234b3bc1673f3ca0866414780a0dbe89f
jameshendricken <jameshendricken@gmail.com>
2025-07-20 19:02:47 +0100

    wed

--- frontend/src/components/Layout.js
+++ frontend/src/components/Layout.js
@@ -4,8 +4,12 @@ export default function Layout({ userEmail, org, onLogout, children }) {
   return (
     <div className="min-h-screen bg-gray-100">
       <header className="w-full bg-white shadow p-4 flex justify-between items-center">
-        <span className="text-sm text-gray-600">Logged in as: <strong>{userEmail}</strong></span>
-        <span className="text-sm text-gray-600">Organisation:  <strong>{orgName}</strong></span>
+        {/* <span className="text-sm text-gray-600">Logged in as: <strong>{userEmail}</strong></span>
+        <span className="text-sm text-gray-600">Organisation:  <strong>{orgName}</strong></span> */}
+        <div>
+          <p className="text-lg font-semibold">{userEmail}</p>
+          <p className="text-sm text-gray-500 italic">Org: {orgName}</p>
+        </div>
         <button
         onClick={onLogout}
         className="px-4 py-2 bg-indigo-600 text-white rounded-md hover:bg-indigo-700 transition"
