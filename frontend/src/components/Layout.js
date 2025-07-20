83795b6299dc3657c626d6c21641afb0e8914bf6
jameshendricken <jameshendricken@gmail.com>
2025-07-20 18:50:46 +0100

    Added org to header

--- frontend/src/components/Layout.js
+++ frontend/src/components/Layout.js
@@ -1,10 +1,11 @@
 import React from "react";
 
-export default function Layout({ userEmail, onLogout, children }) {
+export default function Layout({ userEmail, org, onLogout, children }) {
   return (
     <div className="min-h-screen bg-gray-100">
       <header className="w-full bg-white shadow p-4 flex justify-between items-center">
         <span className="text-sm text-gray-600">Logged in as: <strong>{userEmail}</strong></span>
+        <span className="text-sm text-gray-600">Organisation:  <strong>{org}</strong></span>
         <button
         onClick={onLogout}
         className="px-4 py-2 bg-indigo-600 text-white rounded-md hover:bg-indigo-700 transition"
